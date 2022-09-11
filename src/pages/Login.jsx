1fc33d4a70116cca8626d4c8cc1d31019e5aee81
MubashirWaheed <mubashir3080@gmail.com>
2022-09-12 03:06:53 +0500

    made login page responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import {
   Box,
-  Grid,
   Button,
   Divider,
   Typography,
   Alert,
   Fade,
-  Paper,
   Card,
   CardMedia,
   CardActions,
+  Container,
+  CssBaseline,
 } from "@mui/material";
 import LoginForm from "../components/LoginForm";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate, Link } from "react-router-dom";
+import { AuthCard } from "../utils/styles/AuthCard";
 import { ReactComponent as Google } from "../assets/svgs/google.svg";
 import instagram from "../assets/instagram.png";
 
@@ -41,91 +42,110 @@ const Login = () => {
 
   return (
     <div>
-      <Box
-        display="flex"
-        flexDirection="coloumn"
-        justifyContent="center"
-        alignItems="center"
-        sx={{
-          background: "#fafafa",
-
-          width: "100%",
-        }}
-      >
-        <Card
-          elevation={3}
-          sx={{
-            direction: "column",
-            alignItems: "center",
-            p: 3,
-            mt: 4,
-            backgroundColor: "#fff",
-            width: "375px",
-            minHeight: { xs: "100vh", md: "90vh" },
-          }}
+      <Container sx={{ p: 0 }} background="#fafafa" maxWidth="md">
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          sx={{ width: "100%", height: "100vh" }}
         >
-          <Box
-            display="flex"
-            justifyContent="center"
+          <AuthCard
+            elevation={3}
             sx={{
-              width: "100%",
-              mt: 2,
-              mb: 2,
+              height: {
+                xs: "100vh",
+                sm: "90%",
+              },
             }}
           >
-            <CardMedia
-              sx={{
-                width: 200,
-                maxHeight: { xs: 233, md: 167 },
-                maxWidth: { xs: 350, md: 250 },
-              }}
-              component="img"
-              alt="Instagram logo."
-              src={instagram}
-            />
-          </Box>
-
-          {error && (
-            <Fade
-              in={error}
-              timeout={{ enter: 1000, exit: 2000 }}
-              addEndListener={() => {
-                setTimeout(() => {
-                  setError(false);
-                }, 3000);
-              }}
-            >
-              <Alert severity="error" sx={{ mb: 3, width: "100%" }}>
-                Wrong Credentials
-              </Alert>
-            </Fade>
-          )}
-
-          <LoginForm setError={setError} />
-
-          <Divider sx={{ borderBottomWidth: 35, mt: 2 }}>OR</Divider>
-
-          <CardActions sx={{ display: "flex", flexDirection: "column" }}>
-            <Button
+            <Box
               display="flex"
-              sx={{ textTransform: "none", gap: 1 }}
-              disabled={loading}
-              onClick={handleGoogleLogin}
+              justifyContent="center"
+              sx={{
+                // width: "90%",
+                mt: 2,
+                mb: 2,
+              }}
             >
-              <Google />
-              Login with Google
-            </Button>
+              <CardMedia
+                sx={{
+                  marginTop: 4,
+                  width: 200,
+                  maxHeight: { xs: 233, md: 167 },
+                  maxWidth: { xs: 350, md: 250 },
+                }}
+                component="img"
+                alt="Instagram logo."
+                src={instagram}
+              />
+            </Box>
 
-            <Typography variant="subtitle2" gutterBottom sx={{ mt: 2 }}>
-              Don't have an account? <Link to="/signup">Sign Up</Link>
-            </Typography>
-          </CardActions>
-        </Card>
-      </Box>
+            {error && (
+              <Fade
+                in={error}
+                timeout={{ enter: 1000, exit: 2000 }}
+                addEndListener={() => {
+                  setTimeout(() => {
+                    setError(false);
+                  }, 3000);
+                }}
+              >
+                <Alert severity="error" sx={{ mb: 3, width: "100%" }}>
+                  Wrong Credentials
+                </Alert>
+              </Fade>
+            )}
+
+            <LoginForm setError={setError} />
+            <Divider
+              sx={{
+                mt: 2,
+                // lines not aliging with text properly
+                // width: "100%",
+              }}
+            >
+              Or
+            </Divider>
+
+            <CardActions sx={{ display: "flex", flexDirection: "column" }}>
+              <Button
+                display="flex"
+                sx={{ textTransform: "none", gap: 1 }}
+                disabled={loading}
+                onClick={handleGoogleLogin}
+              >
+                <Google />
+                Login with Google
+              </Button>
+
+              <Typography variant="subtitle2" gutterBottom sx={{ mt: 2 }}>
+                Don't have an account? <Link to="/signup">Sign Up</Link>
+              </Typography>
+            </CardActions>
+            <Box
+              sx={{
+                display: "flex",
+                gap: 1,
+                justifyContent: "center",
+              }}
+              flexWrap="wrap"
+              color="#8e8e8e"
+            >
+              <Typography variant="caption">Meta</Typography>
+              <Typography variant="caption">About</Typography>
+              <Typography variant="caption">Blog</Typography>
+              <Typography variant="caption">Jobs</Typography>
+              <Typography variant="caption">API</Typography>
+              <Typography variant="caption">Privacy</Typography>
+              <Typography variant="caption">Terms</Typography>
+              <Typography variant="caption">Instagram Lite</Typography>
+            </Box>
+          </AuthCard>
+        </Box>
+      </Container>
     </div>
   );
 };
-
-// xs={3} sx={{ }}
 
 export default Login;
